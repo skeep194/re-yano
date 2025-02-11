b28ac833bb32f6131871f67955d7c30b535388bc
JongunJeong <skeep194@knu.ac.kr>
2025-02-11 16:00:00 +0900

    chore: remove unuse command

--- src/commands/lab/ping.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { createSlashCommand, SlashCommand } from '../../../type';
-
-export default createSlashCommand({
-  data: new SlashCommandBuilder().setName('ping').setDescription('pong'),
-  execute: async (interaction) => {
-    interaction.reply('pong');
-  },
-});
