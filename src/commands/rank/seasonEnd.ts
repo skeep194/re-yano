8d2869b2f59b6fedfe174f1dccc6b2b501a78292
JongunJeong <skeep194@knu.ac.kr>
2025-02-11 16:19:03 +0900

    feat: seasonEnd command add season name

--- src/commands/rank/seasonEnd.ts
+++ src/commands/rank/seasonEnd.ts
@@ -13,10 +13,16 @@ export default createSlashCommand({
       interaction.reply('시즌 데이터를 불러오는데 실패했습니다.');
       return;
     }
+    // 정식 출시 첫 시즌이 seasonID 18(프리시즌 1)임.
+    const seasonDelta = 16;
     const seasonEnd = DateTime.fromSQL(currentSeason.seasonEnd);
     const diff = seasonEnd.diffNow(['days', 'hours']);
+    const seasonName =
+      currentSeason.seasonID % 2 === 0
+        ? '프리'
+        : '' + `시즌 ${Math.floor((currentSeason.seasonID - seasonDelta) / 2)}`;
     interaction.reply(
-      `시즌 종료 - ${seasonEnd.toFormat('yyyy년 MM월 dd일 HH시 mm분')}\n${diff.days}일 ${Math.floor(diff.hours)}시간 남음`
+      `${seasonName}\n시즌 종료 - ${seasonEnd.toFormat('yyyy년 MM월 dd일 HH시 mm분')}\n${diff.days}일 ${Math.floor(diff.hours)}시간 남음`
     );
   },
 });
